eb20458a2f58fd67e922eac10b8bb7b726a0e8dd
Krzysztof Staszewski <krzysiek.staszewski@gmail.com>
2023-07-26 16:55:34 +0200

    finished titleClickHandler function to display article after click on title

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
-  console.log('clickedElement (with plus): ' + clickedElement);
+ 
+  
   
   /* [DONE] remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
@@ -11,21 +13,25 @@ for (let activeLink of activeLinks) {
   activeLink.classList.remove('active');
 }
 
-  /* [IN PROGRESS]add class 'active' to the clicked link */
-clickedElement.classList.add('active');
+  /* [DONE]add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+
   /* [DONE] remove class 'active' from all articles */
 const activeArticles = document.querySelectorAll('article.active');
 
 for (let activeArticle of activeArticles) {
   activeArticle.classList.remove('active');
 }
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
-  /* get 'href' attribute from the clicked link */
-
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
-}
+  /* [DONE]find the correct article value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
+/*[IN PROGRESS] add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+} 
 
 const links = document.querySelectorAll('.titles a');
 
