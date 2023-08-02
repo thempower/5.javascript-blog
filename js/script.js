c2b2c67a046e75aafef8d3b80fac55a3840895f8
Krzysztof Staszewski <krzysiek.staszewski@gmail.com>
2023-08-02 14:32:35 +0200

    module_6 init

--- js/script.js
+++ js/script.js
@@ -50,3 +50,23 @@ function generateTitleLinks(){
   }
 }
 generateTitleLinks();
+
+function generateTags(){
+  /* find all articles */
+  const tags = document.querySelectorAll('data-tags');
+  console.log(tags);
+  /* START LOOP: for every article: */
+  /* find tags wrapper */
+  /* make html variable with empty string */
+  /* get tags from data-tags attribute */
+  /* split tags into array */
+  /* START LOOP: for each tag */
+  /* generate HTML of the link */
+  /* add generated code to html variable */
+  /* END LOOP: for each tag */
+  /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
